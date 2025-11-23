74857afe12405a49cbe18c33d32ecc19d4908493
Marcelo Ruenes <marcel-013@hotmail.com>
2025-11-23 11:58:32 -0500

    Se arregla el dominio en el servicio

--- src/main/java/com/nuevaera/backend_spring_nuevaera/controller/AuthController.java
+++ src/main/java/com/nuevaera/backend_spring_nuevaera/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin(origins = "https://nuevaera-v9ts.vercel.app/")
+//@CrossOrigin(origins = "https://nuevaera-v9ts.vercel.app/").
 @CrossOrigin(origins = {
         "https://www.quimicosnuevaera.com",
         "quimicosnuevaera.com",
